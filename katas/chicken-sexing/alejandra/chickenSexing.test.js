7f071afea7714ca21414fc3b1a0f1379dcd76659
Jose Guevara <jose.guevara@fundacion-jala.org>
2021-07-12 03:20:45 -0400

    Add chicken sexing kata and implementation/test files

---
+++ katas/chicken-sexing/alejandra/chickenSexing.test.js
@@ -0,0 +1,7 @@
+import { correctness } from './chickenSexing.js'
+
+test("correctness should find the scoring for guessing chicken sex", () => {
+    /*expect(correctness(['M', 'F', '?'], ['M', 'F', '?'])).toBe(3);
+    expect(correctness(['M', '?', 'M'], ['M', 'F', '?'])).toBe(2);
+    expect(correctness(['F', 'M', 'F'], ['M', 'F', 'M'])).toBe(0);*/
+})
