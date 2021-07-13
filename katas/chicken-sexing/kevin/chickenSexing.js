5455af9734f5c1b573fb7de2c29edc954108d32e
Kevin Herrera <Kevin.Herrera@jala-foundation.org>
2021-07-13 15:17:22 -0400

    Adding code for chickenSexing.js and running tests

--- katas/chicken-sexing/kevin/chickenSexing.js
+++ katas/chicken-sexing/kevin/chickenSexing.js
@@ -1,3 +1,17 @@
 export function correctness(bobsDecisions, expertDecisions) {
 
+    let points = 0;
+
+    for (let i = 0; i <= bobsDecisions.length - 1; i++) {
+        if (bobsDecisions[i] == expertDecisions[i]) {
+            points = points + 1;
+
+        }
+        else if (bobsDecisions[i] == '?' || expertDecisions[i] == '?') {
+            points = points + 0.5;
+
+        }
+    }
+
+    return points;
 }
