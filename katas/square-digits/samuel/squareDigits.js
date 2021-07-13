76edd4a39f6927eb0674227081d55b1c3180a8d2
Samuel Sanchez <Samuel.Sanchez@jala-foundation.org>
2021-07-13 17:58:57 -0500

    Added space after 'if' and before the '{' in all lines required

--- katas/square-digits/samuel/squareDigits.js
+++ katas/square-digits/samuel/squareDigits.js
@@ -2,10 +2,10 @@ export function squareDigits(num) {
     let numero = num.toString();
     let res
     let suma = '';
-    if(num == 0){
+    if (num == 0) {
         return 0;
     }
-    else{
+    else {
         numero.split('').forEach(c => {
             res = Math.pow(parseInt(c),2);
             suma += res.toString();
