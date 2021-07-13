3a09d554c93317b3c930a335c8e88b64893ecf01
Kevin Herrera <Kevin.Herrera@jala-foundation.org>
2021-07-13 19:38:46 -0400

    Removing empty lines, using else-if in the same line of closing braces and changing variable i for index on chickenSexing.js

--- katas/chicken-sexing/kevin/chickenSexing.js
+++ katas/chicken-sexing/kevin/chickenSexing.js
@@ -1,17 +1,11 @@
 export function correctness(bobsDecisions, expertDecisions) {
-
     let points = 0;
-
-    for (let i = 0; i <= bobsDecisions.length - 1; i++) {
-        if (bobsDecisions[i] == expertDecisions[i]) {
+    for (let index = 0; index <= bobsDecisions.length - 1; index++) {
+        if (bobsDecisions[index] == expertDecisions[index]) {
             points = points + 1;
-
-        }
-        else if (bobsDecisions[i] == '?' || expertDecisions[i] == '?') {
+        } else if (bobsDecisions[index] == '?' || expertDecisions[index] == '?') {
             points = points + 0.5;
-
         }
     }
-
     return points;
 }
