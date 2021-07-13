bfbaabec4dd51c1dbbf1886f68e5d01b19f32fc4
Mateo Florez <mateo.florez@jala-foundation.org>
2021-07-13 16:58:54 -0500

    fixed and error in index++ in the correctness function

--- katas/chicken-sexing/mateo/chickenSexing.js
+++ katas/chicken-sexing/mateo/chickenSexing.js
@@ -2,7 +2,7 @@ export function correctness(bobsDecisions, expertDecisions) {
 
     let points = 0;
 
-    for (let index = 0; index < bobsDecisions.length; i++){
+    for (let index = 0; index < bobsDecisions.length; index++){
         if (bobsDecisions[index] == expertDecisions[index]){
             points++;
         } else if (bobsDecisions [index] == '?' || expertDecisions [index] == '?'){
