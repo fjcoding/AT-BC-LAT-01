970a2f11b834fa5d4db7fbf87092c41c11450e3c
Mateo Florez <mateo.florez@jala-foundation.org>
2021-07-13 09:55:05 -0500

    correctness function implemented in chicken-sexing and uncommented chicken sexing test

--- katas/chicken-sexing/mateo/chickenSexing.js
+++ katas/chicken-sexing/mateo/chickenSexing.js
@@ -1,3 +1,14 @@
 export function correctness(bobsDecisions, expertDecisions) {
+    let points = 0;
+    for (let i = 0; i < bobsDecisions.length; i++){
+        if (bobsDecisions[i] == expertDecisions[i]){
+            points++;
+        } else if (bobsDecisions [i] == '?'){
+            points += 0.5;
+        } else if (expertDecisions [i] == '?'){
+            points += 0.5;
+        }
+    }
 
+    return points;
 }
