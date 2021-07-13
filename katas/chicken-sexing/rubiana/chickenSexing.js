2774cb5b2214b01a46a34d1286dff7d302d723ad
Rubiana Perucci <rubiana.perucci@jala-foundation.org>
2021-07-13 19:41:41 -0300

    correctness function updated as per reviewed in class and proper commit message.

--- katas/chicken-sexing/rubiana/chickenSexing.js
+++ katas/chicken-sexing/rubiana/chickenSexing.js
@@ -1,8 +1,7 @@
 export function correctness(bobsDecisions, expertDecisions) {
     var score = 0;
-    var length = bobsDecisions.length;
 
-    for (let index = 0; index < length; index++) {
+    for (let index = 0; index < bobsDecisions.length; index++) {
         if (bobsDecisions[index] == expertDecisions[index]) {
             score += 1;
         } else if (bobsDecisions[index] == '?' || expertDecisions[index] == '?') {
