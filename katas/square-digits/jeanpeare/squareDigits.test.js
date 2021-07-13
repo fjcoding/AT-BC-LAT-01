f96d2ea9deadce57ef55b3e3cfa85485c89ec4c5
Jean Peare Rubio <jeanpeare.rubio@jala-foundation.org>
2021-07-13 12:25:19 -0500

    Problem was solved correctly with some programming strategies.

--- katas/square-digits/jeanpeare/squareDigits.test.js
+++ katas/square-digits/jeanpeare/squareDigits.test.js
@@ -1,8 +1,8 @@
 import { squareDigits } from './squareDigits.js'
 
 test("squareDigits should square each digit and concatenate each result to a final int", () => {
-    expect(squareDigits(0)).toBe(0);
-    /*expect(squareDigits(3212)).toBe(9414);
+    expect(squareDigits(3212)).toBe(9414);
+    expect(squareDigits(3212)).toBe(9414);
     expect(squareDigits(2112)).toBe(4114);
     expect(squareDigits(2501)).toBe(42501);
     expect(squareDigits(5856)).toBe(25642536);
@@ -23,5 +23,5 @@ test("squareDigits should square each digit and concatenate each result to a fin
     expect(squareDigits(3642)).toBe(936164);
     expect(squareDigits(374)).toBe(94916);
     expect(squareDigits(5205)).toBe(254025);
-    expect(squareDigits(7677)).toBe(49364949);*/
+    expect(squareDigits(7677)).toBe(49364949);
 })
