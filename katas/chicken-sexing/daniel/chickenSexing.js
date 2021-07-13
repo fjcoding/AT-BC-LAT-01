87edc6712cadd3000c9c6e128a1b26eac77ea717
Daniel Gomez Betancur <Daniel.Gomez@jala-foundation.org>
2021-07-13 14:27:10 -0500

    The daniel test file is uncomment and the chicken-sexing.js function is developed

--- katas/chicken-sexing/daniel/chickenSexing.js
+++ katas/chicken-sexing/daniel/chickenSexing.js
@@ -1,3 +1,14 @@
 export function correctness(bobsDecisions, expertDecisions) {
+    // There is a conditional for every case observed in the README file
+    let count = 0;
+    for (let i = 0; i < bobsDecisions.length; i++) {  
+        if (bobsDecisions[i] == expertDecisions[i]) {
+            count = count + 1;
+        } else if ((bobsDecisions[i] != expertDecisions[i]) && 
+            ((bobsDecisions[i] == '?') || (expertDecisions[i] == '?'))) {
+            count = count + 0.5;
+        } 
+    }
 
+    return count;
 }
