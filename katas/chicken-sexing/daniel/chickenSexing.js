8261f96612b290acb629a200940c9bce216e29aa
Daniel Gomez Betancur <Daniel.Gomez@jala-foundation.org>
2021-07-13 17:08:29 -0500

    improving code structure

--- katas/chicken-sexing/daniel/chickenSexing.js
+++ katas/chicken-sexing/daniel/chickenSexing.js
@@ -1,14 +1,14 @@
 export function correctness(bobsDecisions, expertDecisions) {
     // There is a conditional for every case observed in the README file
-    let count = 0;
-    for (let i = 0; i < bobsDecisions.length; i++) {  
-        if (bobsDecisions[i] == expertDecisions[i]) {
-            count = count + 1;
-        } else if ((bobsDecisions[i] != expertDecisions[i]) && 
-            ((bobsDecisions[i] == '?') || (expertDecisions[i] == '?'))) {
-            count = count + 0.5;
+    let score = 0;
+    for (let index = 0; index < bobsDecisions.length; index++) {  
+        if (bobsDecisions[index] == expertDecisions[index]) {
+            score = score + 1;
+        } else if ((bobsDecisions[index] != expertDecisions[index]) && 
+            ((bobsDecisions[index] == '?') || (expertDecisions[index] == '?'))) {
+            score = score + 0.5;
         } 
     }
 
-    return count;
+    return score;
 }
