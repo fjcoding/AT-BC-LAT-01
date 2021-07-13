aa37cb0dc9256d08e3d97ae7d53ef237bd9fc2e3
Samuel Sanchez <Samuel.Sanchez@jala-foundation.org>
2021-07-13 18:06:36 -0500

    Changed else if solution to get a simpler one, added space after 'if' conditional and before '{', also changed var names for better understanding

--- katas/chicken-sexing/samuel/chickenSexing.js
+++ katas/chicken-sexing/samuel/chickenSexing.js
@@ -1,18 +1,14 @@
 export function correctness(bobsDecisions, expertDecisions) {
-    let res = 0;
+    let result = 0;
 
-    bobsDecisions.forEach((c, i) => {
-        c = bobsDecisions[i];
-        if(c == expertDecisions[i]){
-            res += 1;
-        }
-        else if(c != expertDecisions[i] && c == '?'){
-            res += 0.5;
-        }
-        else if(c != expertDecisions[i] && expertDecisions[i] == '?'){
-            res += 0.5;
+    bobsDecisions.forEach((character, index) => {
+        character = bobsDecisions[index];
+        if (character == expertDecisions[index]) {
+            result += 1;
+        }else if (character == '?' || expertDecisions[index] == '?') {
+            result += 0.5;
         }
     });
 
-    return res;
+    return result;
 }
