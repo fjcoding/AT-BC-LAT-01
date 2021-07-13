2eb2614b367f480893adc9149c054caff77f929f
Andrés Pinzón <andres.pinzon@jala-foundation.org>
2021-07-13 14:48:06 -0500

    andres' chickensexing.js method implemented

--- katas/chicken-sexing/andres/chickenSexing.js
+++ katas/chicken-sexing/andres/chickenSexing.js
@@ -1,3 +1,11 @@
 export function correctness(bobsDecisions, expertDecisions) {
-
+    var score = 0;
+    for(var i=0; i<bobsDecisions.length; i++){
+        if (bobsDecisions[i]==expertDecisions[i]) {
+            score += 1;
+        }else if(bobsDecisions[i]=='?'||expertDecisions[i]=='?'){
+            score += 0.5;
+        }
+    }
+    return score;
 }
