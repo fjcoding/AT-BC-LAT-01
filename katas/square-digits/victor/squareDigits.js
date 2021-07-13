fb00f35ce053e32212274846c7ff31713f06d0db
Victor Soto <Victor.Soto@jala-foundation.com>
2021-07-13 18:11:25 -0400

    Implemented squareDigit solution, passed all the tests

--- katas/square-digits/victor/squareDigits.js
+++ katas/square-digits/victor/squareDigits.js
@@ -2,11 +2,10 @@ export function squareDigits(num) {
         let  strNum = num.toString();
         let  result = 0;
 
-                for(let i = 0; i < strNum.length; i++){
-                        let squareNum = parseInt(strNum[i]) * parseInt(strNum[i]);
+                for (let index = 0; index < strNum.length; index++){
+                        let squareNum = parseInt(strNum[index]) * parseInt(strNum[index]);
                         result = result + String(squareNum);
                 }
-
 
 
         return parseInt(result);
