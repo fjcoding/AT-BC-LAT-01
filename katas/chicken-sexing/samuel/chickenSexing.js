b1da0ba6e1a4b69d7fa8a26fdcc1a23bda54f7ad
Samuel Sanchez <Samuel.Sanchez@jala-foundation.org>
2021-07-13 10:32:09 -0500

    Uncommented chickenSexing tests and solved function correctness from chickenSexing.js, all tests passed

--- katas/chicken-sexing/samuel/chickenSexing.js
+++ katas/chicken-sexing/samuel/chickenSexing.js
@@ -1,3 +1,18 @@
 export function correctness(bobsDecisions, expertDecisions) {
+    let res = 0;
 
+    bobsDecisions.forEach((c, i) => {
+        c = bobsDecisions[i];
+        if(c == expertDecisions[i]){
+            res += 1;
+        }
+        else if(c != expertDecisions[i] && c == '?'){
+            res += 0.5;
+        }
+        else if(c != expertDecisions[i] && expertDecisions[i] == '?'){
+            res += 0.5;
+        }
+    });
+
+    return res;
 }
