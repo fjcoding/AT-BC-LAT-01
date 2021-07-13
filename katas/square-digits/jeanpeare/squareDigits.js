127424e486fda8fc3498ad0b148d90febef17766
Jean Peare Rubio <jeanpeare.rubio@jala-foundation.org>
2021-07-13 12:21:55 -0500

    Problem was solved correctly with some programming strategies.

--- katas/square-digits/jeanpeare/squareDigits.js
+++ katas/square-digits/jeanpeare/squareDigits.js
@@ -1,3 +1,14 @@
 export function squareDigits(num) {
-    return 0;
+    let div = [...`${num}`].map(c => parseInt(c)); //Divide input number in an array
+    var i = div.length;
+    var j = 0;
+    let resa =[];
+    while (j<i){
+        resa[j] = Math.pow(div[j],2);
+        /*console.log(resa[j],div[j]);*/
+        j++;
+    }
+    var resint = resa.join('');
+    /*console.log(resint);*/
+    return parseInt(resint);
 }
