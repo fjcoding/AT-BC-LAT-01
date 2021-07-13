e87f00b88e95038368b50894b8acd5d7d18c3f49
Rubiana Perucci <rubiana.perucci@jala-foundation.org>
2021-07-13 15:51:22 -0300

    teste

--- katas/chicken-sexing/rubiana/chickenSexing.js
+++ katas/chicken-sexing/rubiana/chickenSexing.js
@@ -1,3 +1,14 @@
 export function correctness(bobsDecisions, expertDecisions) {
+    var final = 0;
+    var length = bobsDecisions.length;
 
+    for (let i = 0; i < length; i++) {
+        if (bobsDecisions[i] == expertDecisions[i]) {
+            final = final + 1;
+        } else if ((bobsDecisions[i] != expertDecisions[i]) && ((bobsDecisions[i] == '?') || (expertDecisions[i] == '?'))) {
+            final = final + 0.5;
+        }
+    }
+
+    return final;
 }
