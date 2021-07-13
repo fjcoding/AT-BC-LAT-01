f2195af321ff87930bd9be3009f4f8e56189db5b
Mateo Florez <mateo.florez@jala-foundation.org>
2021-07-13 16:55:37 -0500

    squareDigits function fixed with the class comments

--- katas/square-digits/mateo/squareDigits.js
+++ katas/square-digits/mateo/squareDigits.js
@@ -1,18 +1,13 @@
-export function squareDigits(num) {
+export function squareDigits(num){
 
     let numeros = num.toString().split('');
     let suma = [];
 
-
-    for (let i = 0; i < numeros.length; i++){
-        suma[i] = parseInt(numeros[i]) * parseInt(numeros[i]);
+    for (let index = 0; index < numeros.length; index++){
+        suma[index] = parseInt(numeros[index]) * parseInt(numeros[index]);
     }
 
-    suma = suma.join();
-
-    for (let i = 0; i < suma.length; i++){
-        suma = suma.replace(',', '');
-    }
+    suma = suma.join('');
 
     return parseInt(suma);
 }
