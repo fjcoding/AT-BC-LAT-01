ed387c56ba8db178940d70056dcc97c29a45252f
Mateo Florez <mateo.florez@jala-foundation.org>
2021-07-12 20:51:33 -0500

    in the test file uncommented and the function squareDigits get solved with test passed

--- katas/square-digits/mateo/squareDigits.js
+++ katas/square-digits/mateo/squareDigits.js
@@ -1,3 +1,20 @@
 export function squareDigits(num) {
-    return 0;
+
+    let numeros = num.toString().split('');
+    let suma = [];
+
+
+    for (let i = 0; i < numeros.length; i++){
+        suma[i] = parseInt(numeros[i]) * parseInt(numeros[i]);
+    }
+
+    suma = suma.join();
+
+    for (let i = 0; i < suma.length; i++){
+        suma = suma.replace(',', '');
+    }
+
+    return parseInt(suma);
 }
+
+
