175731c660cbce0903d33cb4aa865f5610de2a72
Kevin Herrera <Kevin.Herrera@jala-foundation.org>
2021-07-13 01:47:38 -0400

    Adding code for squareDigits.js and running unit tests for squareDigits.test.js

--- katas/square-digits/kevin/squareDigits.js
+++ katas/square-digits/kevin/squareDigits.js
@@ -1,3 +1,12 @@
 export function squareDigits(num) {
-    return 0;
+    num = num.toString();
+    let squareDigits = num.split("");
+    let result = "";
+    squareDigits.forEach(element => {
+        element = Math.pow(element, 2);
+        result = result + "" + element;
+    }
+    );
+    result = Number(result);
+    return result;
 }
