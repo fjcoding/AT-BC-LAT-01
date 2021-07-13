8632176143f8e129c8470877d5b2ac3b8f7db81c
Jean Peare Rubio <jeanpeare.rubio@jala-foundation.org>
2021-07-13 12:26:10 -0500

    Problem was solved correctly with some programming strategies.

--- katas/chicken-sexing/jeanpeare/chickenSexing.js
+++ katas/chicken-sexing/jeanpeare/chickenSexing.js
@@ -1,3 +1,25 @@
 export function correctness(bobsDecisions, expertDecisions) {
-
+    var i = bobsDecisions.length;
+    var j=0;
+    var points = 0;
+    //console.log(i,j,points,bobsDecisions.length,bobsDecisions[j],expertDecisions[j]);
+    while (j<i){
+        if (bobsDecisions[j] == expertDecisions[j]){
+        points = points + 1;
+        }   else if (bobsDecisions[j] != expertDecisions [j]) {
+            if (bobsDecisions[j] == '?'){
+                points = points + 0.5;
+            }
+            else if (expertDecisions[j] == '?') {
+                points = points + 0.5;
+            }
+        }
+        else { 
+            points = points;
+        }
+        j++;
+        //console.log(points);
+    }
+    
+    return points;
 }
