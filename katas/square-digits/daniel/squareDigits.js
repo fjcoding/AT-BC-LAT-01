6ad23fd872cfbec2ccc8d4e8b63e7d768dfd7718
Daniel Gomez Betancur <Daniel.Gomez@jala-foundation.org>
2021-07-13 12:28:50 -0500

    tests were uncommented and function squareDigits.js was completed

--- katas/square-digits/daniel/squareDigits.js
+++ katas/square-digits/daniel/squareDigits.js
@@ -1,3 +1,12 @@
 export function squareDigits(num) {
-    return 0;
+    // The number is parsed to string in order to iterate each digit and square it
+    // The result is a concatenation of the squared numbers
+    const num_str = num.toString();
+    let result = "";
+
+    for (let i=0; i < num_str.length; i++){
+        result = result + (Math.pow(parseInt(num_str[i]),2)).toString();
+    }
+    
+    return parseInt(result);
 }
