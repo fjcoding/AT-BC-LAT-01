ee1f2b17380f1ea267337f6722ce241a9db4dedc
Andrés Pinzón <andres.pinzon@jala-foundation.org>
2021-07-13 22:09:43 -0500

    andres/squareDigits.js implemented and test uncommented

--- katas/square-digits/andres/squareDigits.js
+++ katas/square-digits/andres/squareDigits.js
@@ -1,3 +1,10 @@
 export function squareDigits(num) {
-    return 0;
+    var inputString = String(num);
+    var resultString = "";    
+    for (var index = 0; index < inputString.length; index++) {
+        var valor = Number(inputString[index]);
+        valor *= valor;
+        resultString = resultString + String(valor);
+    }
+    return Number(resultString);
 }
