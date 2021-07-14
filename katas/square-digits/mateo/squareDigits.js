ba54231ac8fe61d9076ce444307cb9e4bcad5f4f
Mateo Florez <mateo.florez@jala-foundation.org>
2021-07-14 13:29:51 -0500

    all changes in chicken sexing eliminated and space between ) and { added for square digits, names changed to english

--- katas/square-digits/mateo/squareDigits.js
+++ katas/square-digits/mateo/squareDigits.js
@@ -1,15 +1,13 @@
-export function squareDigits(num){
+export function squareDigits(num) {
 
-    let numeros = num.toString().split('');
-    let suma = [];
+    let numbers = num.toString().split('');
+    let res = [];
 
-    for (let index = 0; index < numeros.length; index++){
-        suma[index] = parseInt(numeros[index]) * parseInt(numeros[index]);
+    for (let index = 0; index < numbers.length; index++) {
+        res[index] = parseInt(numbers[index]) * parseInt(numbers[index]);
     }
 
-    suma = suma.join('');
+    res = res.join('');
 
-    return parseInt(suma);
+    return parseInt(res);
 }
-
-
