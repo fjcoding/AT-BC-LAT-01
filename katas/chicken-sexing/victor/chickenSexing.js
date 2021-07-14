eb24ccd0c853773525279ea7879049a7157f85d2
Victor Soto <Victor.Soto@jala-foundation.com>
2021-07-14 18:24:42 -0400

    implemented solution for chickenSexing kata using format suggestion on previous kata

--- katas/chicken-sexing/victor/chickenSexing.js
+++ katas/chicken-sexing/victor/chickenSexing.js
@@ -1,3 +1,11 @@
 export function correctness(bobsDecisions, expertDecisions) {
-
+  let scrPoint = 0;
+  for (let index = 0; index < bobsDecisions.length; index++) {
+    if (bobsDecisions[index] === expertDecisions[index]) {
+      scrPoint += 1;
+    } else if (bobsDecisions[index] === '?' || expertDecisions[index] === '?') {
+      scrPoint += 0.5;
+    }
+  }
+  return scrPoint;
 }
