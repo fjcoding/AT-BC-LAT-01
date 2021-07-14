fd71fa17eb36968c2eb179154b2888d551c08084
Victor Soto <Victor.Soto@jala-foundation.com>
2021-07-14 12:38:59 -0400

    reviewd code format

--- katas/square-digits/victor/squareDigits.js
+++ katas/square-digits/victor/squareDigits.js
@@ -1,14 +1,9 @@
 export function squareDigits(num) {
-        let  strNum = num.toString();
-        let  result = 0;
-
-                for (let index = 0; index < strNum.length; index++){
-                        let squareNum = parseInt(strNum[index]) * parseInt(strNum[index]);
-                        result = result + String(squareNum);
-                }
-
-
-        return parseInt(result);
-
+  let strNum = num.toString();
+  let result = 0;
+  for (let index = 0; index < strNum.length; index++) {
+    let squareNum = parseInt(strNum[index]) * parseInt(strNum[index]);
+    result = result + String(squareNum);
+  }
+  return parseInt(result);
 }
-
