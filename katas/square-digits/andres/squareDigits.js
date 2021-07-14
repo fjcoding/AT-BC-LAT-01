87a734d8bd905dbe682655c3149ead84197eac34
Andrés Pinzón <andres.pinzon@jala-foundation.org>
2021-07-13 22:30:36 -0500

    changed variable name in squareDigits.js to comply with naming guidelines

--- katas/square-digits/andres/squareDigits.js
+++ katas/square-digits/andres/squareDigits.js
@@ -2,9 +2,9 @@ export function squareDigits(num) {
     var inputString = String(num);
     var resultString = "";    
     for (var index = 0; index < inputString.length; index++) {
-        var valor = Number(inputString[index]);
-        valor *= valor;
-        resultString = resultString + String(valor);
+        var digit = Number(inputString[index]);
+        digit *= digit;
+        resultString = resultString + String(digit);
     }
     return Number(resultString);
 }
