811ec3a4b1cd1d94d8e3516fa222c429f0f92bb8
Mateo Florez <mateo.florez@jala-foundation.org>
2021-07-14 19:36:20 -0500

    update space between ) { in line 6

--- katas/chicken-sexing/mateo/chickenSexing.js
+++ katas/chicken-sexing/mateo/chickenSexing.js
@@ -3,7 +3,7 @@ export function correctness(bobsDecisions, expertDecisions) {
     let points = 0;
 
     for (let index = 0; index < bobsDecisions.length; index++) {
-        if (bobsDecisions[index] == expertDecisions[index]){
+        if (bobsDecisions[index] == expertDecisions[index]) {
             points++;
         } else if (bobsDecisions[index] == '?' || expertDecisions[index] == '?') {
             points += 0.5;
