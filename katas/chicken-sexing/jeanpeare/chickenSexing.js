f1a14420af7690461e517dbd50e9780d30bcdf43
Jean Peare Rubio <jeanpeare.rubio@jala-foundation.org>
2021-07-16 16:33:40 -0500

    x

--- katas/chicken-sexing/jeanpeare/chickenSexing.js
+++ katas/chicken-sexing/jeanpeare/chickenSexing.js
@@ -1,20 +1,20 @@
 export function correctness(bobsDecisions, expertDecisions) {
-    var i = bobsDecisions.length;                                   // Get the lenght of the options array
-    var j = 0;                                                      // Declare the variable for looping
-    var points = 0;                                                 // Declare the variable for points
-    while (j<i) {
-        if (bobsDecisions[j] == expertDecisions[j]) {               // Validation of the condition with same opinions
-        points = points + 1;                                        // + 1 point
+    var arraylength = bobsDecisions.length;                                  
+    var arrayindex = 0;                                                      
+    var points = 0;                                                          
+    while (arrayindex < arraylength) {
+        if (bobsDecisions[arrayindex] == expertDecisions[arrayindex]) {     
+            points = points + 1;                                             
         }   
-        else if (bobsDecisions[j] != expertDecisions [j]) {         // Validation of the difference between the opinions
-            if (bobsDecisions[j] == '?') {                          // Validation if bob Decision was a doubt
-                points = points + 0.5;                              // +0.5 points
+        else if (bobsDecisions[arrayindex] != expertDecisions [arrayindex]) {          
+            if (bobsDecisions[arrayindex] == '?') {                          
+                points = points + 0.5;                              
             }
-            else if (expertDecisions[j] == '?') {                   // Validation if expert Decision was a doubt
-                points = points + 0.5;                              //+0.5 points
+            else if (expertDecisions[arrayindex] == '?') {                  
+                points = points + 0.5;                              
             }
         }
-        j++;
+        arrayindex++;
     }
-    return points;                                                  // return the total of points got
+    return points;                                                 
 }
