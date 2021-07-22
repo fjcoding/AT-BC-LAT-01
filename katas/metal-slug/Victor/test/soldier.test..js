ba99a0fa2d4bb7fbe51bae7c4b199ea5b4be49c9
Victor Soto <victor.soto@jala-foundation.org>
2021-07-22 18:51:35 -0400

    enables eslint for windows project since i was worling on linux before, so it wasn't active

--- katas/metal-slug/Victor/test/soldier.test..js
+++ katas/metal-slug/Victor/test/soldier.test..js
@@ -3,16 +3,13 @@ import { Soldier } from '../soldier';
 
 describe('Constructor of the soldier class with default values', () => {
     it('should crate an instace of the class', () => {
-
-    let soldier = new Soldier();
-
-    expect(typeof soldier._healthPoints).toBe('number');
-    expect(typeof soldier._lifePoints).toBe('number');
-    expect(typeof soldier._attackPoints).toBe('number');
-    expect(typeof soldier._haveGun).toBe('boolean');
-    expect(typeof soldier._gunType).toBe('string');
-    expect(typeof soldier._onVehicle).toBe('boolean');
-    expect(typeof soldier._vehicleType).toBe('string');
-});
-
+        let soldier = new Soldier();
+        expect(typeof soldier._healthPoints).toBe('number');
+        expect(typeof soldier._lifePoints).toBe('number');
+        expect(typeof soldier._attackPoints).toBe('number');
+        expect(typeof soldier._haveGun).toBe('boolean');
+        expect(typeof soldier._gunType).toBe('string');
+        expect(typeof soldier._onVehicle).toBe('boolean');
+        expect(typeof soldier._vehicleType).toBe('string');
+    });
 });
