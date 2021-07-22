d7d54c09c8d289ce4f9f03d46c4396ea252102a1
Victor Soto <victor.soto@jala-foundation.org>
2021-07-22 19:10:46 -0400

    created character class and soldier sub class, character and soldier test for instances creation with given and default values, configured eslint for windows enviroment that's because i was working on linux before

--- katas/metal-slug/Victor/soldier.js
+++ katas/metal-slug/Victor/soldier.js
@@ -4,7 +4,7 @@ export class Soldier extends Character {
     constructor(healthPoints = 1 ,lifePoints = 3, gunType = 'none', vehicleType = 'none', attackPoints = 0, haveGun = false, onVehicle = false){
         super(healthPoints, attackPoints, haveGun, onVehicle);
         this._lifePoints = Number(lifePoints);
-        this._gunType = String(gunType);
+        this._gunType = String(gunType);      
         this._vehicleType = String(vehicleType);
     }
     
