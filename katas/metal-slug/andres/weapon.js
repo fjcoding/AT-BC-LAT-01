ad6738a3160d22637f15dd48c5898f4669116d6e
Andrés Pinzón <andres.pinzon@jala-foundation.org>
2021-07-22 15:54:21 -0500

    changed file format to LF in weapon.js and its test

---
+++ katas/metal-slug/andres/weapon.js
@@ -0,0 +1,8 @@
+export class Weapon {
+    constructor (name, attackPower, multipleEnemies) {
+        this.weaponName = name;
+        this.weaponAttackPower = attackPower;
+        this.weaponHitsMultiple = multipleEnemies;
+    }
+
+}
