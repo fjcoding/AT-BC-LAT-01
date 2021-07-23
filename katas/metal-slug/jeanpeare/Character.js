8de7c552ef12ad0cb25cf01932e3d34c7d05e33c
Jean Peare Rubio <jeanpeare.rubio@jala-foundation.org>
2021-07-22 19:39:03 -0500

    Files corrected after discrepancy with eslint, package-lock modified after library installation

--- katas/metal-slug/jeanpeare/Character.js
+++ katas/metal-slug/jeanpeare/Character.js
@@ -27,35 +27,35 @@ export class Enemy extends Character{
         this.Armor = Armor;
         this.Abilitie = Abilitie;
     }
-};
+}
 
 export const Soldier = new Enemy(
     'Soldier',
     1,
     0,
     'Dont have'
-)
+);
 
 export const Mercenarie = new Enemy(
     'Mercenarie',
     1,
     2,
     'Dont have'
-)
+);
 
 export const Alien = new Enemy(
     'Alien',
     4,
     0,
     'Invisible'
-)
+);
 
 export const Boss = new Enemy(
     'Boss',
     10,
     10,
     'Heal'
-)
+);
 
 console.log(Enemy);
     
