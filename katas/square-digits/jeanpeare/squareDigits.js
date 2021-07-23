4f9526dcc153eec03e4fed6ca0902b5fcbf66f9f
Jean Peare Rubio <jeanpeare.rubio@jala-foundation.org>
2021-07-23 12:32:53 -0500

    Trying to avoid errors, Branch created again

--- katas/square-digits/jeanpeare/squareDigits.js
+++ katas/square-digits/jeanpeare/squareDigits.js
@@ -1,12 +1,12 @@
 export function squareDigits(num) {
-    let divideArr = [...`${num}`].map(c => parseInt(c)); 
-    var arraylength = divideArr.length;                  
-    var arrayindex = 0;                                  
-    let resultArr = [];                                  
+    let divideArr = [...`${num}`].map(c => parseInt(c));
+    var arraylength = divideArr.length;
+    var arrayindex = 0;
+    let resultArr = [];
     while (arrayindex < arraylength) {
-        resultArr[arrayindex] = Math.pow(divideArr[arrayindex], 2);         
+        resultArr[arrayindex] = Math.pow(divideArr[arrayindex], 2);
         arrayindex++;
     }
-    var resultInt = resultArr.join('');                  
-    return parseInt(resultInt);                          
+    var resultInt = resultArr.join('');
+    return parseInt(resultInt);
 }
