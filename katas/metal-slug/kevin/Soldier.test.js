842773679b82902c80c585052052f0b12d15e49d
Kevin Herrera <Kevin.Herrera@jala-foundation.org>
2021-07-23 16:34:46 -0400

    Adding changes on unit tests on Soldier.test.js

--- katas/metal-slug/kevin/Soldier.test.js
+++ katas/metal-slug/kevin/Soldier.test.js
@@ -1,7 +1,20 @@
 import { Soldier } from './Soldier.js';
-test('Verify that the soldier was created correctly with his actions', () => {
-    const soldier = new Soldier(true, true, true); 
-    expect(soldier.shootWeapon).toBe(true);
-    expect(soldier.useKnife).toBe(true);
-    expect(soldier.throwGrenades).toBe(true);
+import { HeavyMachineGun } from './weapons/HeavyMachineGun.js';
+test('Verify that the soldier was created correctly with his attributes', () => {
+    let soldier = new Soldier(1, 1, 3, 1);
+    const expectedSoldierLifePoints = 1;
+    const expectedSoldierAttackPower = 1;
+    const expectedSoldierLives = 3;
+    const expectedSoldierDistance = 1;
+    expect(soldier.lifePoints).toBe(expectedSoldierLifePoints);
+    expect(soldier.attackPower).toBe(expectedSoldierAttackPower);
+    expect(soldier.lives).toBe(expectedSoldierLives);
+    expect(soldier.distance).toBe(expectedSoldierDistance);
+});
+test('Verify that the soldier can shoot a HeavyMachineGun', () => {
+    let soldier = new Soldier(1, 1, 3, 1);
+    let heavyMG = new HeavyMachineGun(4);
+    const actualResult = soldier.shootAWeapon(heavyMG);
+    const expectedSoldierTotalAttackPower = 2;
+    expect(actualResult).toBe(expectedSoldierTotalAttackPower);
 });
