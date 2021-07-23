4d3f43399e80c9bb1f9958142f0318bcba350e98
Jose Guevara <jguevara@pros.com>
2021-07-23 01:25:46 -0400

    Fix lint issue enforcing use of camelcase

--- katas/square-digits/daniel/squareDigits.js
+++ katas/square-digits/daniel/squareDigits.js
@@ -1,11 +1,11 @@
 export function squareDigits(num) {
     // The number is parsed to string in order to iterate each digit and square it
     // The result is a concatenation of the squared numbers
-    const num_str = num.toString();
+    const numStr = num.toString();
     let result = '';
 
-    for (let index = 0; index < num_str.length; index++) {
-        result = result + (Math.pow(parseInt(num_str[index]), 2)).toString();
+    for (let index = 0; index < numStr.length; index++) {
+        result = result + (Math.pow(parseInt(numStr[index]), 2)).toString();
     }
 
     return parseInt(result);
