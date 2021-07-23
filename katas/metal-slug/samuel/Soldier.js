3f039163fa3296aa51d40589f9457c2cc4ebce43
Samuel Sanchez <Samuel.Sanchez@jala-foundation.org>
2021-07-23 15:21:15 -0500

    Designed test for method changeWeapon modified using open-close principle
    -Implemented method changeWeapon using objects as arguments and using
    open-close principle, test passed.

--- katas/metal-slug/samuel/Soldier.js
+++ katas/metal-slug/samuel/Soldier.js
@@ -8,10 +8,22 @@ export class Soldier {
         this.lifes = 3;
     }
 
-    changeWeapon(weaponAttackPower) {
-        this.attackPower = 1;
-        this.attackPower = this.attackPower + weaponAttackPower;
-        return this.attackPower;
+    changeWeapon(weaponInstance, soldierInstance) {
+        let weaponEntries = Object.entries(weaponInstance);
+        let soldierEntries = Object.entries(soldierInstance);
+        for (let index = 0; index < weaponEntries.length; index++) {
+            let weaponAttribute = weaponEntries[index];
+            let weaponAttributeName = weaponAttribute[0];
+            let weaponAttributeValue = weaponAttribute[1];
+            for (let soldierIndex = 0; soldierIndex < soldierEntries.length; soldierIndex++) {
+                let soldierAttributes = soldierEntries[soldierIndex];
+                let soldierAttributeName =  soldierAttributes[0];
+                if (soldierAttributeName == weaponAttributeName) {
+                    soldierInstance[weaponAttributeName] += weaponAttributeValue;
+                }
+            }
+        }
+        return soldierInstance;
     }
 
     useKnife(civilianOrEnemy) {
--- katas/metal-slug/samuel/Soldier.test.js
+++ katas/metal-slug/samuel/Soldier.test.js
@@ -1,14 +1,41 @@
 import { Soldier } from './Soldier';
 import { Civilian } from './Civilian';
 import { Enemy } from './Enemy';
+import { RocketLauncher } from './Weapons';
+import { Grenade } from './Weapons';
+import { FlameShot } from './Weapons';
+import { Shotgun } from './Weapons';
 
 describe ('When a Soldier instance', () => {
     test ('changes weapon, his attackPower will increase', () => {
-        const soldierInstance = new Soldier();
-        expect(soldierInstance.changeWeapon(5)).toBe(6);
-        expect(soldierInstance.changeWeapon(2)).toBe(3);
-        expect(soldierInstance.changeWeapon(4)).toBe(5);
-        expect(soldierInstance.changeWeapon(10)).toBe(11);
+        const soldierInstance1 = new Soldier();
+        const shotgunInstance = new Shotgun();
+        expect(soldierInstance1.changeWeapon(shotgunInstance, soldierInstance1)).toEqual({
+            resistance: 1,
+            attackPower: 5,
+            lifes: 3
+        });
+        const soldierInstance2 = new Soldier();
+        const flameShotInstance = new FlameShot();
+        expect(soldierInstance2.changeWeapon(flameShotInstance, soldierInstance2)).toEqual({
+            resistance: 1,
+            attackPower: 5,
+            lifes: 3
+        });
+        const soldierInstance3 = new Soldier();
+        const grenadeInstance = new Grenade();
+        expect(soldierInstance3.changeWeapon(grenadeInstance, soldierInstance3)).toEqual({
+            resistance: 1,
+            attackPower: 10,
+            lifes: 3
+        });
+        const soldierInstance4 = new Soldier();
+        const rocketLauncherInstance = new RocketLauncher();
+        expect(soldierInstance4.changeWeapon(rocketLauncherInstance, soldierInstance4)).toEqual({
+            resistance: 1,
+            attackPower: 4,
+            lifes: 3
+        });
     });
     test ('use a knife, the action will change depending on the object', () => {
         const soldierInstance = new Soldier();
