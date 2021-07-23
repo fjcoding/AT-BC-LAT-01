49da1c68545e33ecaab14b90d4cce6b7a81a31f0
Andrés Pinzón <andres.pinzon@jala-foundation.org>
2021-07-23 15:24:05 -0500

    Added method weapon.dealDamage() and test soldier takes massive damage

--- katas/metal-slug/andres/soldier.test.js
+++ katas/metal-slug/andres/soldier.test.js
@@ -1,31 +1,40 @@
 import { Soldier } from './soldier.js';
 import { Weapon } from './weapon.js';
 
+const canHitOneTarget = false;
+const handgunAttackPower = 1;
+const mainCharacterHitPoints = 1;
+const machinegunAttackPower = 3;
+const maxPossibleDamage = 65531; 
+//const planeHitPoints = 4; //For use in test Plain takes damage
+
 test('Create a new soldier, equiped with Handgun', () => {
-    const handgunAttackPower = 1;
-    const mainCharacterHitpoints = 1;
-    const handgun = new Weapon('Handgun',handgunAttackPower,false);
-    const obj = new Soldier(handgun,'Marco',mainCharacterHitpoints);
+    const handgun = new Weapon('Handgun',handgunAttackPower,canHitOneTarget);
+    const obj = new Soldier(handgun,'Marco',mainCharacterHitPoints);
     expect(obj.isAlive()).toBe(true);
 });
-test('Soldier gets a machine gun', () => {
-    const handgunAttackPower = 1;
-    const machinegunAttackPower = 3;
-    const mainCharacterHitpoints = 1;
-    const handgun = new Weapon('Handgun',handgunAttackPower,false);
-    const machinegun = new Weapon('Machine Gun',machinegunAttackPower, true);
-    const obj = new Soldier(handgun,'Fio',mainCharacterHitpoints);
+test('Soldier gets a heavy machine gun', () => {
+    const handgun = new Weapon('Handgun',handgunAttackPower,canHitOneTarget);
+    const machinegun = new Weapon('Heavy Machine Gun',machinegunAttackPower, canHitOneTarget);
+    const obj = new Soldier(handgun,'Fio',mainCharacterHitPoints);
     obj.equipNewWeapon(machinegun);
     expect(obj.attackWithEquippedWeapon()).toBe(machinegunAttackPower);
 });
 /*
-test('Plane takes damage from an enemy handgun', () => {
-    return something;
-});
-test('Soldier takes massive damage', () => {
-    return something;
-});
-test('Insert test description here xD', () => {
-    return something;
+test('Plain takes damage from an enemy handgun', () => {
+    const handgun = new Weapon('Handgun',handgunAttackPower,canHitOneTarget);
+    const machinegun = new Weapon('Heavy Machine Gun',machinegunAttackPower, canHitOneTarget);
+    const damagedPlainHitPoints = planeHitPoints - machinegunAttackPower;
+    //there's no weapon specified for vehicles, so I'm giving it a HMG
+    const obj = new Soldier(machinegun,'Plain',planeHitPoints);
+    obj.takeDamage(handgun.dealsDamage());
+    expect(obj.currentHealth).toBe(damagedPlainHitPoints); //HELP! It receives 3 parameters. Needs a different matcher (or different logic)
 });
 */
+test('Soldier takes massive damage', () => {
+    const handgun = new Weapon('Handgun',handgunAttackPower,canHitOneTarget);
+    const obj = new Soldier(handgun,'Marco',mainCharacterHitPoints);
+    obj.takeDamage(maxPossibleDamage); //soon to rise to test boundary values
+    obj.takeDamage(handgunAttackPower);
+    expect(obj.isAlive()).toBe(false);
+});
--- katas/metal-slug/andres/weapon.test.js
+++ katas/metal-slug/andres/weapon.test.js
@@ -7,3 +7,8 @@ test('create shotgun object successfully', () => {
     expect(obj.weaponAttackPower).toBe(shotgunAttackPower);
     expect(obj.weaponHitsMultiple).toBe(true);
 });
+test('rocket launcher deals damage', () => {
+    const rocketLauncherAttackPower = 6;
+    const obj = new Weapon('Rocket Launcher', rocketLauncherAttackPower, false);
+    expect(obj.dealsDamage()).toBe(rocketLauncherAttackPower);
+});
