dd69f4f23b67541a6f7b63815bce739f0d2e6c21
Rubiana Perucci <rubiana.perucci@jala-foundation.org>
2021-07-26 19:15:02 -0300

    Errors with the number format oh the health attribute

--- katas/metal-slug/rubiana/characters.test.js
+++ katas/metal-slug/rubiana/characters.test.js
@@ -11,12 +11,20 @@ test('isAlive() return true if characters health is bigger than 0', () => {
     expect(soldado.isAlive()).toBe(true);
 });
 
-test('useVehicle() adds the vehicle to the character and increase its health with the vehicles health points', () => {
-    soldado.useVehicle(plane.vehicle, plane.vehicleHealth);
-    expect(soldado.vehicle).toBe('Plane');
-});
-
 test('useWeapon() adds the weapon to the character and increase its attack power with the weapons attack power', () => {
     soldado.useWeapon(shotgun.weapon, shotgun.attackPower);
     expect(soldado.weapon).toBe('Shotgun');
+    expect(soldado.attackPower).toBe(6);
+
+});
+
+test('useVehicle() adds the vehicle to the character and increase its health with the vehicles health points', () => {
+    soldado.useVehicle(plane.vehicle, plane.vehicleHealth);
+    expect(soldado.vehicle).toBe('Plane');
+    expect(soldado.health).toBe(5);
+});
+
+test('getDamage() decreases the characters health with the amout of damaged it took', () => {
+    soldado.getDamage(3)
+    expect(soldado.health).toBe(2);
 });
