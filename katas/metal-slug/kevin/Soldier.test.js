0d99d5b71012c7b6bc7b503367659600d08fd7a0
Kevin Herrera <zkev_lo@hotmail.com>
2021-07-27 11:38:33 -0400

    revert of the commit before rebase

--- katas/metal-slug/kevin/Soldier.test.js
+++ katas/metal-slug/kevin/Soldier.test.js
@@ -11,6 +11,18 @@ test('Verify that the soldier was created correctly with his attributes', () => 
     expect(soldier.lives).toBe(expectedSoldierLives);
     expect(soldier.distance).toBe(expectedSoldierDistance);
 });
+test('Verify that the soldier recognize a close enemy', () => {
+    let soldier = new Soldier(1, 1, 3, 1);
+    let enemyDistance = 0;
+    const actualResult = soldier.isEnemyclose(enemyDistance);
+    expect(actualResult).toBe(true);
+});
+test('Verify that the soldier recognize an enemy vehicle', () => {
+    let soldier = new Soldier(1, 1, 3, 1);
+    let tankLifePoints = 3;
+    const actualResult = soldier.isEnemyAVehicle(tankLifePoints);
+    expect(actualResult).toBe(true);
+});
 test('Verify that the soldier can use a Knife with a non-enemy  close vehicle', () => {
     let soldier = new Soldier(1, 1, 3, 1);
     const actualResult = soldier.useAKnife(false, true);
