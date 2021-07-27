09b632059933b5285fc7e3911a3a7e4cc9a151d9
Rubiana Perucci <rubiana.perucci@jala-foundation.org>
2021-07-27 17:26:42 -0300

    REST API endpoint and route to scenarios created. Also implemented simple messages to each Request (GET, POST and PUT).

---
+++ src/main/routes/scenarios.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling GET requests to API/scenarios'
+    });
+});
+
+router.post('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling POST requests to API/scenarios'
+    });
+});
+
+router.put('/:objID', (req, res, next) => {
+    const id = req.params.objID;
+    res.status(200).json({
+        message: 'Handling PUT requests with a specified Object ID to API/scenarios',
+        id: id
+    });
+});
+
+module.exports = router;
