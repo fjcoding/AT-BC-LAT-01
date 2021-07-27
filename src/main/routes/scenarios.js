037e36c522fbc295a6c741b0314d087a8bc41f1f
Rubiana Perucci <rubiana.perucci@jala-foundation.org>
2021-07-27 17:32:11 -0300

    fixing lint error

--- src/main/routes/scenarios.js
+++ src/main/routes/scenarios.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
     res.status(200).json({
         message: 'Handling GET requests to API/scenarios'
     });
 });
 
-router.post('/', (req, res, next) => {
+router.post('/', (req, res) => {
     res.status(200).json({
         message: 'Handling POST requests to API/scenarios'
     });
 });
 
-router.put('/:objID', (req, res, next) => {
+router.put('/:objID', (req, res) => {
     const id = req.params.objID;
     res.status(200).json({
         message: 'Handling PUT requests with a specified Object ID to API/scenarios',
