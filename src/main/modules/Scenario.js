28ac577ac326bcea12bb1d512b0d0d672efafc0e
Kevin Herrera <zkev_lo@hotmail.com>
2021-07-28 16:54:29 -0400

    fixing eslint issues

--- src/main/modules/Scenario.js
+++ src/main/modules/Scenario.js
@@ -1,22 +1,21 @@
 import { Actors } from './actor.js';
 
 export class Scenario {
-    constructor (id){
+    constructor(id) {
         this.id = id;
     }
+
     createActor(actorObjects) {
-            console.log(Object.keys(actorObjects) + "AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH");
-            actorObjects.forEach(function (, index) {
+        actorObjects.forEach(function (value, index) {
             actorObjects[index].name = new Actors(actorObjects[index].name, actorObjects[index].type, actorObjects[index].weapon);
-            console.log(index + actorObjects[index] + "LLALALALALAALLALALA");
             return actorObjects[index].name;
         });
-        
+
     }
 
     createActions(actionObjects) {
         actionObjects.forEach(function (value, index) {
-            actionObjects[index].name = new Actors(actionObjects[index].name, actorObjects[index].type, actorObjects[index].weapon);
+            actionObjects[index].name = new Actors(actionObjects[index].name, actionObjects[index].type, actionObjects[index].weapon);
         });
 
     }
