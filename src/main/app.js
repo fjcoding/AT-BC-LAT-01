718fca72d96d614e2770c282dbcacb1c5d02ad4a
Daniel Gomez Betancur <Daniel.Gomez@jala-foundation.org>
2021-07-29 10:59:12 -0500

    http methods deleted

--- src/main/app.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-const scenariosRoutes = require('./routes/scenarios');
-
-app.use('/scenarios', scenariosRoutes);
-
-module.exports = app;
