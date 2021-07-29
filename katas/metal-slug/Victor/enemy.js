329da5fbd19691349e680e8901eb3a1d4ec9c4a3
Victor Soto <victor.soto@jala-foundation.org>
2021-07-28 22:21:50 -0400

    remove values from enemy class

--- katas/metal-slug/Victor/enemy.js
+++ katas/metal-slug/Victor/enemy.js
@@ -1,9 +1,9 @@
 import { Character } from './character.js';
 import { weaponsList } from './weaponList.js';
 export class Enemy extends Character {
-    constructor(healthPoints = 1, enemyType = 'footSoldier',
-        gunType = weaponsList._weaponType.handGun, abilityType = 'throwGranade',
-        vehicleType = 'none', attackPoints = weaponsList._weaponType.handGun._attackPower, haveWeapon = true, onVehicle = false){
+    constructor(healthPoints, enemyType = 'footSoldier',
+        gunType, abilityType,
+        vehicleType, attackPoints, haveWeapon, onVehicle){
         super(healthPoints, attackPoints, haveWeapon, onVehicle, gunType, vehicleType);
         this._enemyType = String(enemyType);
         this._abilityType = String(abilityType);
