e6a2065b8246c74f8c0e5adb3212c7e5bca34018
Samuel Sanchez <Samuel.Sanchez@jala-foundation.org>
2021-07-30 11:59:30 -0500

    Deleted unnecessary files and reorganized databaseLibrary

--- src/main/DB/db.js
+++
@@ -1,8 +0,0 @@
-import * as admin from 'firebase-admin';
-
-admin.initializeApp({
-    credential: admin.credential.applicationDefault(),
-    databaseURL: 'https://metal-slug-maker-default-rtdb.firebaseio.com'
-});
-
-export const db = admin.firestore();
