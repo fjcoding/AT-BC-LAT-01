3591f9007d1b34a5ea5753ca8f539ddc1f141368
Daniel Gomez Betancur <Daniel.Gomez@jala-foundation.org>
2021-08-03 12:18:49 -0500

    creation of ScenarioHandler class with tests

---
+++ src/main/api-utilities/ScenarioHandler.js
@@ -0,0 +1,13 @@
+export class ScenarioHandler {
+    constructor (scenario = {}) {
+        this.scenario = scenario;
+    }
+
+    pushAttribute (newObj, attribute = '') {
+        if (Array.isArray(this.scenario[attribute]) == true) {
+            this.scenario[attribute].push(newObj);
+        } else {
+            this.scenario[attribute] = [newObj];
+        }
+    }
+}
---
+++ src/test/api-utilities/ScenarioHandler.test.js
@@ -0,0 +1,73 @@
+import { ScenarioHandler } from '../../main/api-utilities/ScenarioHandler';
+
+describe('Verify that ScenarioHandler', () => {
+    test('has scenario attribute', () => {
+        const scenarioHandler = new ScenarioHandler();
+        expect(scenarioHandler.scenario).not.toBe(undefined);
+    });
+
+    test('is created with an scenario as an input', () => {
+        const scenario = {
+            actors: [],
+            actions: []
+        };
+        const scenarioHandler = new ScenarioHandler(scenario);
+        expect(scenarioHandler.scenario).toBe(scenario);
+    });
+
+    test('pushes a new actor in the actors array', () => {
+        const scenario = {
+            actors: [
+                {name: 'Marco', type: 'PF Squad Soldier', health: 1},
+                {name: 'RAS1', type: 'Rebel Army soldier', health: 1}
+            ],
+        };
+        const scenarioHandler = new ScenarioHandler(scenario);
+        const newActor = {name: 'RAS2', type: 'Rebel Army soldier', health: 2};
+
+        scenarioHandler.pushAttribute(newActor, 'actors');
+        expect(scenarioHandler.scenario.actors).toEqual([
+            {name: 'Marco', type: 'PF Squad Soldier', health: 1},
+            {name: 'RAS1', type: 'Rebel Army soldier', health: 1},
+            {name: 'RAS2', type: 'Rebel Army soldier', health: 2}
+        ]);
+    });
+
+    test('pushes and creates a new actors array attribute', () => {
+        const scenarioHandler = new ScenarioHandler();
+        const newActor = {name: 'RAS2', type: 'Rebel Army soldier', health: 2};
+
+        scenarioHandler.pushAttribute(newActor, 'actors');
+        expect(scenarioHandler.scenario.actors).toEqual([
+            {name: 'RAS2', type: 'Rebel Army soldier', health: 2}
+        ]);
+    });
+
+    test('pushes a new action in the actions array', () => {
+        const scenario = {
+            actions: [
+                {actor: 'Marco', use: 'weapon', target: 'east'},
+                {actor: 'RAS1', use: 'weapon', target: 'west'}
+            ],
+        };
+        const scenarioHandler = new ScenarioHandler(scenario);
+        const newAction = {actor: 'RAS2', use: 'weapon', target: 'west'};
+
+        scenarioHandler.pushAttribute(newAction, 'actions');
+        expect(scenarioHandler.scenario.actions).toEqual([
+            {actor: 'Marco', use: 'weapon', target: 'east'},
+            {actor: 'RAS1', use: 'weapon', target: 'west'},
+            {actor: 'RAS2', use: 'weapon', target: 'west'}
+        ]);
+    });
+
+    test('pushes and creates a new actions array attribute', () => {
+        const scenarioHandler = new ScenarioHandler();
+        const newAction = {name: 'RAS2', type: 'Rebel Army soldier', health: 2};
+
+        scenarioHandler.pushAttribute(newAction, 'actions');
+        expect(scenarioHandler.scenario.actions).toEqual([
+            {name: 'RAS2', type: 'Rebel Army soldier', health: 2}
+        ]);
+    });
+});
