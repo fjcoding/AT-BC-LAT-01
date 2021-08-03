4069bf582b6a36219ea88577b37f1fb31e9e5b21
Samuel Sanchez <Samuel.Sanchez@jala-foundation.org>
2021-08-03 09:32:04 -0500

    Created mock folder with mock function and dessigned tests
    -Until i found how to return the promise, it's returning a string

---
+++ src/test/db/db.test.js
@@ -0,0 +1,36 @@
+// eslint-disable-next-line jest/no-mocks-import
+import { db } from '../../main/__mocks__/firebase-mock';
+//global.db = firebaseMock();
+
+//import { db } from '../../main/server';
+
+describe('Database connection', () => {
+    test('should add a json and return the id', () => {
+        let res = db
+            .collection()
+            .add({
+                actors: [{
+                    name: 'Marco',
+                    type: 'PF Squad Soldier',
+                    weapon: 'Handgun'
+                }, {
+                    name: 'RAS1',
+                    type: 'Rebel Army soldier',
+                    weapon: 'rifle'
+                }],
+                actions: [{
+                    actor: 'Marco',
+                    action: 'Pick Shotgun',
+                    element: 'Shotgun'
+                }, {
+                    actor: 'Marco',
+                    action: 'Shoot Weapon',
+                    element: 'Shotgun'
+                }, {
+                    actor: 'RAS1',
+                    action: 'receive attack',
+                    from: 'Marco'
+                }]});
+        expect(res).toBe('BFDUCchwAQXCXLq4AAxp');
+    });
+});
