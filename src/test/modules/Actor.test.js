1869a4faf9caf899681d4dcc2493a1d8faef5126
Jean Peare Rubio <jeanpeare.rubio@jala-foundation.org>
2021-08-05 15:10:43 -0500

    Integrating all cases in unit test for Shooting action

--- src/test/modules/Actor.test.js
+++ src/test/modules/Actor.test.js
@@ -3,11 +3,14 @@ import  { Actor } from '../../main/modules/Actor.js';
 describe('class constructor should recibe data to build a Actors', () => {
 
     test('The instance attributes need to be returned as they are defined', () => {
-        const actor = new Actor('RAS1', 'Rebel Army soldier', 'tank cannon', 1);
+        const actor = new Actor('RAS1', 'Rebel Army soldier', 'tank cannon', 1, {xPos: 0, yPos: 1});
         expect(actor.name).toBe('RAS1');
         expect(actor.health).toBe(1);
         expect(actor.type).toBe('Rebel Army soldier');
         expect(actor.weapon).toBe('tank cannon');
+        expect(actor.position).toEqual({'xPos': 0, 'yPos': 1});
+        expect(actor.position.xPos).toEqual(0);
+        expect(actor.position.yPos).toEqual(1);
     });
 
     test('Methods getValue and setValue need to set and get the value expected in the correct attribute', () => {
--- src/test/modules/Execution.test.js
+++ src/test/modules/Execution.test.js
@@ -1,62 +1,91 @@
 import { Execution } from '../../main/modules/Execution.js';
 
 var parserActionssArray = [
-/*    {'actionActor': 'Marco', 'actionType': 'Pick Weapon', 'element': 'Handgun'},
-    {'actionActor': 'Marco', 'actionType': 'Heal', 'element': 5},
-    {'actionActor': 'RAT1', 'actionType': 'Receive Attack', 'target': 'Marco'},*/
-    {'actionActor': 'RAT1', 'actionType': 'Shoot Weapon', 'target': 'south'},
-    /*    {'actionActor': 'Marco', 'actionType': 'Receive Attack', 'element': 'RAS1'},
-    {'actionActor': 'RAS1', 'actionType': 'Receive Attack', 'element': 'Marco'},*/
-    {}];
+    {'actionActor': 'Marco', 'actionType': 'Shoot Weapon', 'target': 'east'},
+    {'actionActor': 'Marco', 'actionType': 'Shoot Weapon', 'target': 'west'},
+    {'actionActor': 'RAS1', 'actionType': 'Shoot Weapon', 'target': 'north'},
+    {'actionActor': 'RAT1', 'actionType': 'Shoot Weapon', 'target': 'south'}];
 
 var parserActorssArray = [
-    {'health': 10, 'name': 'Marco', 'type': 'PF Squad Soldier', 'weapon': {
-        'name': 'rifle',
-        'power': 10,
-        'xScope': 10,
-        'yScope': 10
-    }, 'position': {
-        'xPos': 10,
-        'yPos': 0
-    }},
-    {'health': 1, 'name': 'RAS1', 'type': 'Rebel Army soldier', 'weapon': 'rifle', 'position': {
-        'xPos': 9,
-        'yPos': 6
-    }},
-    {'health': 2, 'name': 'RAT1', 'type': 'Rebel Army Tank', 'weapon': {
-        'name': 'rifle',
-        'power': 10,
-        'xScope': 10,
-        'yScope': 7
-    }, 'position': {
-        'xPos': 1,
-        'yPos': 8
-    }}];
+    {'health': 10,
+        'name': 'Marco',
+        'type': 'PF Squad Soldier',
+        'weapon': {
+            'name': 'handgun',
+            'power': 1,
+            'xScope': 1,
+            'yScope': 1
+        },
+        'position': {
+            'xPos': 1,
+            'yPos': 16
+        }},
+    {'health': 2,
+        'name': 'RAS1',
+        'type': 'Rebel Army Soldier',
+        'weapon': {
+            'name': 'shotgun',
+            'power': 5,
+            'xScope': 2,
+            'yScope': 2
+        },
+        'position': {
+            'xPos': 2,
+            'yPos': 14
+        }},
+    {'health': 2,
+        'name': 'RAT1',
+        'type': 'Rebel Army Tank',
+        'weapon': {
+            'name': 'Rocket Launcher',
+            'power': 10,
+            'xScope': 10,
+            'yScope': 10
+        }, 'position': {
+            'xPos': 0,
+            'yPos': 15
+        }}];
 
 const testExecution = new Execution ();
 
 test('The execution of scenario need to return the actual status of the actors instances after actions being executed', () => {
     expect(testExecution.execute(parserActionssArray, parserActorssArray)).toEqual([
-        {'health': 10, 'name': 'Marco', 'type': 'PF Squad Soldier', 'weapon': {
-            'name': 'rifle',
-            'power': 10,
-            'xScope': 10,
-            'yScope': 10
-        }, 'position': {
-            'xPos': 10,
-            'yPos': 0
-        }},
-        {'health': -9, 'name': 'RAS1', 'type': 'Rebel Army soldier', 'weapon': 'rifle',  'position': {
-            'xPos': 9,
-            'yPos': 6
-        }},
-        {'health': 2, 'name': 'RAT1', 'type': 'Rebel Army Tank', 'weapon': {
-            'name': 'rifle',
-            'power': 10,
-            'xScope': 10,
-            'yScope': 7
-        }, 'position': {
-            'xPos': 1,
-            'yPos': 8
-        }}]);
+        {'health': 5,
+            'name': 'Marco',
+            'type': 'PF Squad Soldier',
+            'weapon': {
+                'name': 'handgun',
+                'power': 1,
+                'xScope': 1,
+                'yScope': 1
+            }, 'position': {
+                'xPos': 1,
+                'yPos': 16
+            }},
+        {'health': -9,
+            'name': 'RAS1',
+            'type': 'Rebel Army Soldier',
+            'weapon': {
+                'name': 'shotgun',
+                'power': 5,
+                'xScope': 2,
+                'yScope': 2
+            },
+            'position': {
+                'xPos': 2,
+                'yPos': 14
+            }},
+        {'health': -4,
+            'name': 'RAT1',
+            'type': 'Rebel Army Tank',
+            'weapon': {
+                'name': 'Rocket Launcher',
+                'power': 10,
+                'xScope': 10,
+                'yScope': 10
+            }, 'position': {
+                'xPos': 0,
+                'yPos': 15
+            }
+        }]);
 });
