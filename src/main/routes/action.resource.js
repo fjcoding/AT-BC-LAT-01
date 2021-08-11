0002bf1c6dc77afce7682d1ab4f642d5636c43a8
Rubiana Perucci <rubiana.perucci@jala-foundation.org>
2021-08-11 10:57:23 -0300

    Changed the resources files that used QH.get() to remove the .data()

--- src/main/routes/action.resource.js
+++ src/main/routes/action.resource.js
@@ -6,29 +6,29 @@ import { Runner } from '../modules/Runner';
 const router = express.Router();
 
 export default function(QueryHandler) {
-    router.put('/', async (req, res) => {
-        if (!req.body.scenario) res.send({status: 400, error: 'No scenario specidied'});
+    router.put('/', async(req, res) => {
+        if (!req.body.scenario) res.send({ status: 400, error: 'No scenario specidied' });
         const idScenario = req.body.scenario;
         const action = req.body;
 
         const scenario = await QueryHandler.get(idScenario);
-        if (scenario.data()) {
-            const verifier = new VerifierInterface(scenario.data(), 'action');
+        if (scenario) {
+            const verifier = new VerifierInterface(scenario, 'action');
 
             var response = verifier.check(action);
             if (response == true) {
-                const handler = new ScenarioHandler(scenario.data());
+                const handler = new ScenarioHandler(scenario);
                 delete action.scenario;
                 console.log(handler.scenario);
                 handler.pushAttribute(action, 'actions');
                 // console.log(handler.scenario);
                 await QueryHandler.set(idScenario, handler.scenario);
-                res.send({code: 202, scenario: handler.scenario});
+                res.send({ code: 202, scenario: handler.scenario });
             }
 
-            res.send({code: 400, error: response});
+            res.send({ code: 400, error: response });
         }
-        res.send({status: 400, error: 'Scenario does not exist'});
+        res.send({ status: 400, error: 'Scenario does not exist' });
     });
 
     //POST:scenarioID
@@ -42,21 +42,21 @@ export default function(QueryHandler) {
         "use": "weapon",
         "target": "west"
     }*/
-    router.post('/:scenarioID', async (req, res) => {
+    router.post('/:scenarioID', async(req, res) => {
         const scenarioID = req.params.scenarioID;
         const scenario = await QueryHandler.get(scenarioID);
-        if (scenario.data()) {
-            const verifier = new VerifierInterface(scenario.data(), 'action');
+        if (scenario) {
+            const verifier = new VerifierInterface(scenario, 'action');
             if (verifier.check(req.body) == true) {
                 const runner = new Runner();
-                console.log(scenario.data().actors);
+                console.log(scenario.actors);
                 console.log(req.body);
-                const result = runner.follow(scenario.data().actors, [req.body]);
+                const result = runner.follow(scenario.actors, [req.body]);
                 res.send({ code: 202, result: result });
             }
-            res.send({status: 400, error: verifier.check(req.body)});
+            res.send({ status: 400, error: verifier.check(req.body) });
         }
-        res.send({status: 400, error: 'Scenario does not exist'});
+        res.send({ status: 400, error: 'Scenario does not exist' });
     });
 
     return router;
