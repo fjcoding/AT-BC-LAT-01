fdc1f8c85bed6432f09a09e8b1cda7cd91f98a9c
Andrés Pinzón <andres.pinzon@jala-foundation.org>
2021-08-12 18:06:37 -0500

    updated the json example in dbTestConstants.js to match the 3rd iteration format

--- src/test/db/dbTestConstants.js
+++ src/test/db/dbTestConstants.js
@@ -2,56 +2,137 @@
 export const notExistingScenarioID = 'thisisnotascenarioid';
 
 export const testScenarioData = {
-    actors: [{
-        name: 'Marco',
-        type: 'PF Squad Soldier',
-        weapon: 'Handgun'
+    'scenes': 5,
+    'actors': [{
+        'name': 'Marco',
+        'type': 'PF Squad Soldier',
+        'health': 1,
+        'weapon': {
+            'name': 'handgun',
+            'power': 1,
+            'xScope': 1,
+            'yScope': 1
+        },
+        'position': {
+            'xPos': 0,
+            'yPos': 0
+        }
     }, {
-        name: 'RAS1',
-        type: 'Rebel Army soldier',
-        weapon: 'rifle'
+        'name': 'Polo',
+        'type': 'PF Squad Soldier',
+        'health': 1,
+        'weapon': {
+            'name': 'handgun',
+            'power': 1,
+            'xScope': 1,
+            'yScope': 1
+        },
+        'position': {
+            'xPos': 0,
+            'yPos': 1
+        }
+    }, {
+        'name': 'RAS1',
+        'type': 'Rebel Army soldier',
+        'health': 1,
+        'weapon': {
+            'name': 'rifle',
+            'power': 1,
+            'xScope': 1,
+            'yScope': 1
+        },
+        'position': {
+            'xPos': 5,
+            'yPos': 0
+        }
+    }, {
+        'name': 'RAS2',
+        'type': 'Rebel Army soldier',
+        'health': 1,
+        'weapon': {
+            'name': 'rifle',
+            'power': 1,
+            'xScope': 1,
+            'yScope': 1
+        },
+        'position': {
+            'xPos': 5,
+            'yPos': 1
+        }
     }],
-    actions: [{
-        actor: 'Marco',
-        action: 'Pick Shotgun',
-        element: 'Shotgun'
-    }, {
-        actor: 'Marco',
-        action: 'Shoot Weapon',
-        element: 'Shotgun'
-    }, {
-        actor: 'RAS1',
-        action: 'receive attack',
-        from: 'Marco'
+    'actions': [{
+        'actor': 'Marco',
+        'action': 'Shot Weapon',
+        'target': 'east',
+        'scenes': 100
     }]
 };
+
 export const testScenarioID = '00kEXu7aRQr35ymqAqH2';
 
 export const updatedMockQueryData = {
-    actors: [{
-        name: 'Marco',
-        type: 'PF Squad Soldier',
-        weapon: 'Handgun'
+    'scenes': 4,
+    'actors': [{
+        'name': 'Marco',
+        'type': 'PF Squad Soldier',
+        'health': 1,
+        'weapon': {
+            'name': 'handgun',
+            'power': 1,
+            'xScope': 1,
+            'yScope': 1
+        },
+        'position': {
+            'xPos': 1,
+            'yPos': 1
+        }
     }, {
-        name: 'RAS1',
-        type: 'Rebel Army soldier',
-        weapon: 'rifle'
+        'name': 'Polo',
+        'type': 'PF Squad Soldier',
+        'health': 1,
+        'weapon': {
+            'name': 'handgun',
+            'power': 1,
+            'xScope': 1,
+            'yScope': 1
+        },
+        'position': {
+            'xPos': 0,
+            'yPos': 1
+        }
     }, {
-        name: 'RAS2',
-        type: 'Rebel Army soldier',
-        weapon: 'shotgun'
+        'name': 'RAS1',
+        'type': 'Rebel Army soldier',
+        'health': 1,
+        'weapon': {
+            'name': 'rifle',
+            'power': 1,
+            'xScope': 1,
+            'yScope': 1
+        },
+        'position': {
+            'xPos': 5,
+            'yPos': 0
+        }
+    }, {
+        'name': 'RAS2',
+        'type': 'Rebel Army soldier',
+        'health': 1,
+        'weapon': {
+            'name': 'rifle',
+            'power': 1,
+            'xScope': 1,
+            'yScope': 1
+        },
+        'position': {
+            'xPos': 5,
+            'yPos': 1
+        }
     }],
-    actions: [{
-        actor: 'Marco',
-        action: 'Pick Shotgun',
-        element: 'Shotgun'
-    }, {
-        actor: 'Marco',
-        action: 'Shoot Weapon',
-        element: 'Shotgun'
-    }, {
-        actor: 'RAS1',
-        action: 'receive attack',
-        from: 'Marco'
+    'actions': [{
+        'actor': 'Marco',
+        'action': 'Shot Weapon',
+        'target': 'east',
+        'scenes': 100
     }]
 };
