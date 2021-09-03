5950e77cd8421cd448c3cc84462b9b9e4ae9feb7
Daniel Gomez Betancur <daniel33.gomez@gmail.com>
2021-09-03 08:59:21 -0500

    add sudo permission to run docker commands in deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -3,24 +3,24 @@
 cd $HOME/metal-slug-maker
 
 echo "Retrieving the Container ID if exits"
-container=$(docker ps --filter status=running --filter name=msm -q)
+container=$(sudo docker ps --filter status=running --filter name=msm -q)
 
 if [ ! -z $container ];
 then
     echo "Container = $container"
     echo "Retrieving the Image Name associated to the container"
-    imageName=$(docker inspect --format='{{.Config.Image}}' $container)
+    imageName=$(sudo docker inspect --format='{{.Config.Image}}' $container)
     echo "ImageName = $imageName"
     if [ $imageName ];
     then
-        imageID=$(docker inspect --format='{{.Id}}' $imageName)
+        imageID=$(sudo docker inspect --format='{{.Id}}' $imageName)
         if [ $imageID ];
         then
-            docker rm -f $container
-            docker rmi -f $imageID
-            docker pull $FULL_IMAGE_NAME:$TAG
+            sudo docker rm -f $container
+            sudo docker rmi -f $imageID
+            sudo docker pull $FULL_IMAGE_NAME:$TAG
         fi
     fi
 fi
 # Deploying
-docker run --name msm -p 3000:3000 -d -v $HOME/keys:/keys/ $FULL_IMAGE_NAME:$TAG
+sudo docker run --name msm -p 3000:3000 -d -v $HOME/keys:/keys/ $FULL_IMAGE_NAME:$TAG
