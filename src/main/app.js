b1e42215c2fda15230661231d03b5986bc8a37fe
Daniel Gomez Betancur <daniel33.gomez@gmail.com>
2021-09-06 20:30:26 -0500

    uncomment actors resourse invocations

--- src/main/app.js
+++ src/main/app.js
@@ -1,12 +1,12 @@
 import express, { json, urlencoded } from 'express';
 import resourceScenario from './routes/scenario.resource.js';
-// import resourceActor from './routes/actor.resource.js';
+import resourceActor from './routes/actor.resource.js';
 import resourceAction from './routes/action.resource.js';
 import resourceWeapon from './routes/weapon.resource.js';
 
 export default function(QueryHandler) {
     const routerScenario = resourceScenario(QueryHandler);
-    // const routerActor = resourceActor(QueryHandler);
+    const routerActor = resourceActor(QueryHandler);
     const routerAction = resourceAction(QueryHandler);
     const routerWeapon = resourceWeapon(QueryHandler);
 
@@ -21,7 +21,7 @@ export default function(QueryHandler) {
 
     //Routes
     app.use('/scenario', routerScenario);
-    // app.use('/actor', routerActor);
+    app.use('/actor', routerActor);
     app.use('/action', routerAction);
     app.use('/weapon', routerWeapon);
 
